02d0455f7165180c7f3d1d1c518870eca11a15c8
Matt Meloncon <matt@MattAdmins-MacBook-Pro.local>
2015-07-19 17:48:04 -0400

    Added FlexSlider smart package and lots of images for the slider. Also added the flex slider fonts, the HTML for the slider, and the JS for it too.

--- client/portfolio.js
+++ client/portfolio.js
@@ -2,4 +2,24 @@ $(document).ready(function(){
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
     $(".button-collapse").sideNav();
+    $('.materialboxed').materialbox();
+    $('.slider').slider({height: 400});
+
+    $("#carousel").flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        asNavFor: "#slider"
+    });
+
+    $(".flexslider").flexslider({
+        animation: "slide",
+        controlNav: false,
+        animationLoop: false,
+        slideshow: false,
+        sync: "#carousel"
+    });
 });
